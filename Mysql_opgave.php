9555afc09a1983ecb8781b39e5c8e6d77903e12f
jakobhoiras <fjz601@alumni.ku.dk>
2014-12-02 22:39:10 +0100

    Debugged Assignment mysql

--- Mysql_opgave.php
+++ Mysql_opgave.php
@@ -11,25 +11,30 @@ class Mysql_assignment {
     }
 
     function create_assignment($name) {
-        $query = "SELECT name FROM Assignments.Assignments WHERE name = ?";
+        $query = "SELECT * FROM Assignments.Assignments";
 
         $formIn = $_POST['formIn'];
         $formOut = $_POST['formIn'];
 
         if ($stmt = $this->conn->prepare($query)) {
-            $stmt->bind_param('s', $un);
             $stmt->execute();
             $result = $stmt->get_result();
-            $result2 = $result->fetch_array(MYSQLI_NUM);
-            echo strtolower($result2[0]);
-
-            if (strtolower($result2[0]) === strtolower($name)) {
+            if ($result = $result->fetch_all()) {
+                $stmt->close();
+            }
+            $already_exists = false;
+            for ($i = 0; $i < sizeof($result); $i++) {
+                if ($result[$i][0] == $name) {
+                    $already_exists = true;
+                }
+            }
+            if ($already_exists == false) {
                 return 1;
             } else {
                 $query = 'INSERT INTO Assignments.Assignments(name) VALUES' . '(?)';
                 $name2 = strtolower($name);
                 if ($stmt = $this->conn->prepare($query)) {
-                    
+
                     $stmt->bind_param('s', $name2);
                     $stmt->execute();
                     $stmt->close();
